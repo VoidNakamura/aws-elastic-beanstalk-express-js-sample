c323313cbce0ae5fa12332314fedd6e2921d031d
VoidNakamura <91678104+VoidNakamura@users.noreply.github.com>
2021-09-30 13:39:47 -0400

    Update app.js
    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Kero Kero'));
+app.get('/', (req, res) => res.send('Rainy season hero: froppy'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
